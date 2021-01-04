11871ff362225f22eb6f01d9a3c5ce28fddc84fc
sangeetds <sangeetnarayands@gmail.com>
2021-01-04 05:51:19 +0530

    Initial Commit

---
+++ interview_question/Customer.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class Customer
+{
+    private $id;
+    private $name;
+
+    /**
+     * Customer constructor.
+     * @param $id
+     * @param $name
+     */
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+}
---
+++ interview_question/SalesPerson.php
@@ -0,0 +1,70 @@
+<?php
+
+
+class SalesPerson
+{
+    private $id;
+    private $name;
+    private $active;
+
+    /**
+     * SalesPerson constructor.
+     * @param $id
+     * @param $name
+     * @param $active
+     */
+    public function __construct(int $id, string $name, bool $active)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->active = $active;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param bool $active
+     */
+    public function setActive(bool $active)
+    {
+        $this->active = $active;
+    }
+}
